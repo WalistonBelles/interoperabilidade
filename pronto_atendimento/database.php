2d7f64196cc5d77c85b1b694bb1e82063f81d14f
Waliston Belles <walistonbelles@bol.com.br>
2020-12-18 18:44:01 -0300

    Atualizando

--- pronto_atendimento/database.php
+++ pronto_atendimento/database.php
@@ -5,10 +5,4 @@
     $password = ""; //Se estiver em localhost e o banco de dados não possuir senha, deixe em branco.
     // Cria Conexão
     $conn = mysqli_connect($servername, $username, $password, $database);
-    // Confere a Conexão
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    echo "Connected successfully";
-    mysqli_close($conn);
 ?>
